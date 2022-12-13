7cadf26aecea3b737a1957aaa6f3bdcab64a2b0d
stephen.hua <stephen.hua@meetsocial.com>
2022-12-13 14:39:17 +0800

    doc: db up

--- src/pages/atoms.tsx
+++ src/pages/atoms.tsx
@@ -22,7 +22,7 @@ export const userAtom = refresh(
   abortableAtom(async (get, { signal }) => {
     const userId = get(incAtom);
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${userId}?_delay=2000`,
+      `https://my-json-server.typicode.com/gitivon/jotai-helper/users`,
       { signal }
     );
     return response.json();
